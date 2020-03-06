f085eea8ef08702308e889d6c209c45f1d799f3c
Ridwan Hady <ridwanhady31@gmail.com>
2020-03-06 22:15:39 +0700

    Add post to submit url

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/ShortUrl');
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({fullUrl: req.body.fullUrl});
+
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT || 5000);
