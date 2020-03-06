876f70697967db856b5faa8a45190f3b76a5e37a
Ridwan Hady <ridwanhady31@gmail.com>
2020-03-06 22:31:20 +0700

    Add redirection on click

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ app.post('/shortUrls', async (req, res) => {
   res.redirect('/');
 });
 
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl});
+
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.fullUrl);
+});
+
 app.listen(process.env.PORT || 5000);
 
 console.log('Server started!');
